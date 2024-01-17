e56c3b70e026cc19d4a7e4a8bdfc9bf3b3c7098e
Abhi-wolf <abhijeetkr2020@outlook.com>
2024-01-17 12:03:36 +0530

    added code for large inputs

--- Recursion/Pascal_triangle.cpp
+++ Recursion/Pascal_triangle.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define ll long long
-
+#define ll long long int
 
 //  not feasible to large inputs
 ll fact(ll n)
@@ -38,44 +37,66 @@ vector<ll> nthRowOfPascalTriangle(int n)
     return vec;
 }
 
-
-
 //  for small and medium inputs
 
-ll fun(int n,int r) {
-    if(n==r || r==0)
+ll fun(int n, int r)
+{
+    if (n == r || r == 0)
         return 1;
-    
-    return fun(n-1,r-1)+fun(n-1,r);
+
+    return fun(n - 1, r - 1) + fun(n - 1, r);
 }
-vector<ll> nthPascal(int n) {
-    vector<ll>vec;
-    n=n-1;
+vector<ll> nthPascal(int n)
+{
+    vector<ll> vec;
+    n = n - 1;
     // vec.push_back(1);
-    for(int i=0;i<=n;i++) {
-        vec.push_back(fun(n,i));
+    for (int i = 0; i <= n; i++)
+    {
+        vec.push_back(fun(n, i));
     }
     return vec;
 }
 
-
 // for large inputs
+vector<vector<ll>> pascalDP(int n)
+{
+    vector<vector<ll>> dp(n, vector<ll>(n, 0));
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (i == j || j == 0)
+                dp[i][j] = 1;
+            else
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+        }
+    }
 
-
-
+    return dp;
+}
 
 int main()
 {
     vector<ll> ans;
     cout << fact(24) << endl;
-    cout<<combination(24,1);
-    ans = nthPascal(50);
+    cout << combination(24, 1);
+    ans = nthPascal(15);
     cout << "\n\n Answer ";
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << "     ";
     }
     cout << "\n\n";
+
+    vector<vector<ll>> ans2 = pascalDP(15);
+    for (int i = 0; i < ans2.size(); i++)
+    {
+        for (int j = 0; j < ans2.size(); j++)
+            cout << ans2[i][j] << ", ";
+
+        cout << endl;
+    }
     return 0;
 }
