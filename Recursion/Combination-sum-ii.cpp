973fe7bbd2be19c455b1be764071147d09ba80ab
Abhi-wolf <abhijeetkr2020@outlook.com>
2024-01-24 12:38:35 +0530

    Combination-sum-ii

--- Recursion/Combination-sum-ii.cpp
+++ Recursion/Combination-sum-ii.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 /*
@@ -26,6 +27,41 @@ Constraints:
 
 */
 
+//  FIRST METHOD (NOT SUITABLE FOR LARGER INPUTS)  (SET IS USED TO STORE UNIQUE VALUES)
+void Solve(int index, int target, vector<int> arr, vector<int> vec, set<vector<int>> &st)
+{
+    if (target == 0)
+        st.insert(vec);
+
+    if (target < 0 || index >= arr.size())
+        return;
+
+    // taken
+    int tar1 = target;
+    vector<int> v1 = vec;
+    tar1 -= arr[index];
+    v1.push_back(arr[index]);
+    Solve(index + 1, tar1, arr, v1, st);
+
+    // not taken
+    Solve(index + 1, target, arr, vec, st);
+}
+
+vector<vector<int>> combinationSum11(vector<int> &candidates, int target)
+{
+    vector<vector<int>> ans;
+    vector<int> vec;
+    set<vector<int>> st;
+    sort(candidates.begin(), candidates.end());
+    Solve(0, target, candidates, vec, st);
+
+    for (auto x : st)
+        ans.push_back(x);
+
+    return ans;
+}
+
+// SECOND METHOD (BETTER) HERE WE ARE PASSING SORTED ARRAY
 void findCombination(int ind, int target, vector<int> arr, vector<vector<int>> &ans, vector<int> &ds)
 {
     if (target == 0)
@@ -36,19 +72,19 @@ void findCombination(int ind, int target, vector<int> arr, vector<vector<int>> &
 
     for (int i = ind; i < arr.size(); i++)
     {
-        if (i > ind && arr[i] == arr[i - 1])
+        if (i > ind && arr[i] == arr[i - 1]) // IF(PREVIOUSLY ADDED NO IS SAME THEN SKIP IT)
             continue;
 
-        if (arr[i] > target)
-            break;
+        if (arr[i] > target) // IF CURRENT ELEMENT IS GREATER THAN TARGET THEN ELEMENTS AFTER THIS WOULD
+            break;           // ALSO BE GREATER BECAUSE THE ARRAY IS SORTED
 
-        ds.push_back(arr[i]);
+        ds.push_back(arr[i]); // TAKE THE NUMBER
         findCombination(i + 1, target - arr[i], arr, ans, ds);
-        ds.pop_back();
+        ds.pop_back(); // BACKTRACK
     }
 }
 
-vector<vector<int>> combinationSum1(vector<int> &candidates, int target)
+vector<vector<int>> combinationSum12(vector<int> &candidates, int target)
 {
     vector<vector<int>> ans;
     vector<int> ds;
@@ -71,7 +107,17 @@ int main()
 {
     vector<int> candiadtes = {2, 5, 2, 1, 2};
     candiadtes = {10, 1, 2, 7, 6, 1, 5};
-    vector<vector<int>> res = combinationSum1(candiadtes, 8);
+    candiadtes = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+    cout << "\nFirst Method \n";
+    vector<vector<int>> res = combinationSum11(candiadtes, 27);
     DisplayArray(res);
+
+    cout << "\n\nSecond Method\n";
+
+    res.clear();
+    res = combinationSum12(candiadtes, 27);
+    DisplayArray(res);
+
     return 0;
 }
